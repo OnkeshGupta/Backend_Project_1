7e678c37008e600fc4e0e8eca6848cf7621fe9e3
Onkesh Gupta <onkeshgupta@gmail.com>
2025-09-06 22:36:55 +0530

    utils code

--- src/index.js
+++ src/index.js
@@ -4,23 +4,11 @@ import connectDB from "./db/index.js";
 
 
 connectDB()
-
-/*
-import express from "express";
-const app = express();
-( async () => {
-    try {
-        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
-        app.on("error", () => {
-            console.log("Error:", error);
-            throw error;
+    .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+            console.log(`Server is running at port : ${process.env.PORT}`);
         })
-        app.listen(process.env.PORT, () => {
-            console.log(`App is listening on port ${process.env.PORT}`);
-        })
-    } catch (error) {
-        console.error("Error:", error);
-        throw error;
-    }
-})();
-*/
+    })
+    .catch((err) => {
+        console.log("MongoDB connection failed !!!", err);
+    })
